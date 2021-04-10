18abbfe995f4cd49fedca8b026c5d9bfbf37eede
Ash <ash.suzuki.r1@gmail.com>
2021-04-10 18:31:26 +0900

    Support CDN_TOKEN & HTTP_X_FORWARDED_FOR

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,11 @@
 class RootController < ApplicationController
   def show
-    Log.create(remote_ip: request.remote_ip, useragent: request.user_agent)
+    remote_ip = if ENV['CDN_TOKEN'].present? && request.env['CDN_TOKEN'] == ENV['CDN_TOKEN']
+      request.env['HTTP_X_FORWARDED_FOR']
+    else
+      request.remote_ip
+    end
+    Log.create(remote_ip: remote_ip, useragent: request.user_agent)
 
     ActionCable.server.broadcast(
       "counter",
